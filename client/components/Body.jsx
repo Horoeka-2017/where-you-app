bde6ff347cf391ad2f584a8fc20cdab6accf6652
joshua-fogg <joshuafoggnz@gmail.com>
2017-08-04 10:21:00 +1200

    added components

---
+++ client/components/Body.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+import Body from './Body'
+
+class Body extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      playerScore: 0,
+      apiData: {}
+    }
+  }
+
+  render () {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default Body
---
+++ client/components/Score.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Score extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      playerScore: 0,
+      apiData: {}
+    }
+  }
+
+  render () {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default Score
