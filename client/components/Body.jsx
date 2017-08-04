a742b4e132a411bf82238ea6d5fe521868c25ca7
joshua-fogg <joshuafoggnz@gmail.com>
2017-08-04 15:58:39 +1200

    checked boxes work

--- client/components/Body.jsx
+++ client/components/Body.jsx
@@ -8,72 +8,129 @@ class Body extends Component {
         {
           id: 1,
           question: 'Which region is the Philippines located?',
-          answers: [{ qid: 'a', answer: 'Africa' }, { qid: 'b', answer: 'Africa' }, { qid: 'c', answer: 'Africa' }, { qid: 'd', answer: 'Africa' }]
+          answers: [
+            { qid: 'a', answer: 'Africa', checked: false, correct: false },
+            { qid: 'b', answer: 'Africa', checked: false, correct: false },
+            { qid: 'c', answer: 'Africa', checked: false, correct: false },
+            { qid: 'd', answer: 'Africa', checked: false, correct: false }
+          ]
         },
         {
           id: 2,
           question: 'What is the capital city of the Philippines?',
-          answers: [{ qid: 'a', answer: 'Africa' }, { qid: 'b', answer: 'Africa' }, { qid: 'c', answer: 'Africa' }, { qid: 'd', answer: 'Africa' }]
+          answers: [
+            { qid: 'a', answer: 'Africa', checked: false, correct: false },
+            { qid: 'b', answer: 'Africa', checked: false, correct: false },
+            { qid: 'c', answer: 'Africa', checked: false, correct: false },
+            { qid: 'd', answer: 'Africa', checked: false, correct: false }
+          ]
         },
         {
           id: 3,
           question: 'What languages are spoken in the Philippines?',
-          answers: [{ qid: 'a', answer: 'Africa' }, { qid: 'b', answer: 'Africa' }, { qid: 'c', answer: 'Africa' }, { qid: 'd', answer: 'Africa' }]
+          answers: [
+            { qid: 'a', answer: 'Africa', checked: false, correct: false },
+            { qid: 'b', answer: 'Africa', checked: false, correct: false },
+            { qid: 'c', answer: 'Africa', checked: false, correct: false },
+            { qid: 'd', answer: 'Africa', checked: false, correct: false }
+          ]
         },
         {
           id: 4,
           question: 'What currency is used in the Philippines?',
-          answers: [{ qid: 'a', answer: 'Africa' }, { qid: 'b', answer: 'Africa' }, { qid: 'c', answer: 'Africa' }, { qid: 'd', answer: 'Africa' }]
+          answers: [
+            { qid: 'a', answer: 'Africa', checked: false, correct: false },
+            { qid: 'b', answer: 'Africa', checked: false, correct: false },
+            { qid: 'c', answer: 'Africa', checked: false, correct: false },
+            { qid: 'd', answer: 'Africa', checked: false, correct: false }
+          ]
         },
         {
           id: 5,
           question: 'What is the latitude and longitude for the Philippines?',
-          answers: [{ qid: 'a', answer: 'Africa' }, { qid: 'b', answer: 'Africa' }, { qid: 'c', answer: 'Africa' }, { qid: 'd', answer: 'Africa' }]
+          answers: [
+            { qid: 'a', answer: 'Africa', checked: false, correct: false },
+            { qid: 'b', answer: 'Africa', checked: false, correct: false },
+            { qid: 'c', answer: 'Africa', checked: false, correct: false },
+            { qid: 'd', answer: 'Africa', checked: false, correct: false }
+          ]
         },
         {
           id: 6,
           question: 'What are the people of the Philippines also referenced as?',
-          answers: [{ qid: 'a', answer: 'Africa' }, { qid: 'b', answer: 'Africa' }, { qid: 'c', answer: 'Africa' }, { qid: 'd', answer: 'Africa' }]
+          answers: [
+            { qid: 'a', answer: 'Africa', checked: false, correct: false },
+            { qid: 'b', answer: 'Africa', checked: false, correct: false },
+            { qid: 'c', answer: 'Africa', checked: false, correct: false },
+            { qid: 'd', answer: 'Africa', checked: false, correct: false }
+          ]
         },
         {
           id: 7,
           question: 'What is the sub-region in which the Philippines is located?',
-          answers: [{ qid: 'a', answer: 'Africa' }, { qid: 'b', answer: 'Africa' }, { qid: 'c', answer: 'Africa' }, { qid: 'd', answer: 'Africa' }]
+          answers: [
+            { qid: 'a', answer: 'Africa', checked: false, correct: false },
+            { qid: 'b', answer: 'Africa', checked: false, correct: false },
+            { qid: 'c', answer: 'Africa', checked: false, correct: false },
+            { qid: 'd', answer: 'Africa', checked: false, correct: false }
+          ]
         },
         {
           id: 8,
           question: 'What is the calling code for the Philippines?',
-          answers: [{ qid: 'a', answer: 'Africa' }, { qid: 'b', answer: 'Africa' }, { qid: 'c', answer: 'Africa' }, { qid: 'd', answer: 'Africa' }]
+          answers: [
+            { qid: 'a', answer: 'Africa', checked: false, correct: false },
+            { qid: 'b', answer: 'Africa', checked: false, correct: false },
+            { qid: 'c', answer: 'Africa', checked: false, correct: false },
+            { qid: 'd', answer: 'Africa', checked: false, correct: false }
+          ]
         },
         {
           id: 9,
           question: 'How does one say the Philippines in German?',
-          answers: [{ qid: 'a', answer: 'Africa' }, { qid: 'b', answer: 'Africa' }, { qid: 'c', answer: 'Africa' }, { qid: 'd', answer: 'Africa' }]
+          answers: [
+            { qid: 'a', answer: 'Africa', checked: false, correct: false },
+            { qid: 'b', answer: 'Africa', checked: false, correct: false },
+            { qid: 'c', answer: 'Africa', checked: false, correct: false },
+            { qid: 'd', answer: 'Africa', checked: false, correct: false }
+          ]
         },
         {
           id: 10,
           question: 'What do they put on the end of their URL?',
-          answers: [{ qid: 'a', answer: 'Africa' }, { qid: 'b', answer: 'Africa' }, { qid: 'c', answer: 'Africa' }, { qid: 'd', answer: 'Africa' }]
+          answers: [
+            { qid: 'a', answer: 'Africa', checked: false, correct: false },
+            { qid: 'b', answer: 'Africa', checked: false, correct: false },
+            { qid: 'c', answer: 'Africa', checked: false, correct: false },
+            { qid: 'd', answer: 'Africa', checked: false, correct: false }
+          ]
         }
-      ],
-      questionCheckState: [
-        { a: false, b: false, c: false, d: false },
-        { a: false, b: false, c: false, d: false },
-        { a: false, b: false, c: false, d: false },
-        { a: false, b: false, c: false, d: false },
-        { a: false, b: false, c: false, d: false },
-        { a: false, b: false, c: false, d: false },
-        { a: false, b: false, c: false, d: false },
-        { a: false, b: false, c: false, d: false },
-        { a: false, b: false, c: false, d: false },
-        { a: false, b: false, c: false, d: false }
+        // Dons Recommendation put Q's into a single JS final js file. retain hecked state.
       ]
     }
     this.toggleChange = this.toggleChange.bind(this)
   }
-  toggleChange () {
+
+  toggleChange (questionId, answerId, newValue) {
+    const newState = this.state.questions.map((question) => {
+      if (question.id === questionId) {
+        const newQVar = Object.assign({}, question)
+        newQVar.answers = newQVar.answers.map((answer) => {
+          if (answer.qid === answerId) {
+            const newAnswer = Object.assign({}, answer)
+            newAnswer.checked = newValue
+            return newAnswer
+          } else {
+            return answer
+          }
+        })
+        return newQVar
+      } else {
+        return question
+      }
+    })
     this.setState({
-      isChecked: !this.state.isChecked
+      questions: newState
     })
   }
 
@@ -93,8 +150,10 @@ class Body extends Component {
                           <label>
                             <input
                               type="checkbox"
-                              checked={this.state.isChecked}
-                              onChange={this.toggleChange}
+                              checked={answer.checked}
+                              onChange={(e) => {
+                                this.toggleChange(singleQuestion.id, answer.qid, !answer.checked)
+                              }}
                             />
                             {answer.answer}
                           </label>
