322e6d22490e9bd5f117f1a9341a2ba22239c733
joshua-fogg <joshuafoggnz@gmail.com>
2017-08-04 16:50:32 +1200

    josh did stuff

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='header'>
       <div className='img-class'><img src='../global.png' height='200px' /></div>
-      <div className='title'><h1>Where You App?!</h1></div>
+      <div className='title'><h1>Where You App?!</h1><br /> API Data Powered by:<br /> https://market.mashape.com/ajayakv/rest-countries</div>
     </div>
   )
 }
