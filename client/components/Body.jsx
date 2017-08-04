1961fe12662cf7731ecb910eb7cf45ca53391c8e
sethman6 <sethnitin6@gmil.com>
2017-08-04 12:00:36 +1200

    body working

--- client/components/Body.jsx
+++ client/components/Body.jsx
@@ -1,22 +1,20 @@
 import React, { Component } from 'react'
 
-import Body from './Body'
-
 class Body extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
       questions: [
 
         {
-          id: 1, question: 'Which region is the Philippines located?',
-          answers: {
-            id: '', answer1: '', answer2: '', answer3: ''
-          }
+          id: 1,
+          question: 'Which region is the Philippines located?',
+          answers: [{ qid: '1', answer: '' }, { qid: '2', answer: '' }, { qid: '3', answer: '' }, { qid: '4', answer: '' }]
         },
 
         {
-          id: 2, question: 'What is the capital city of the Philippines?',
+          id: 2,
+          question: 'What is the capital city of the Philippines?',
           answers:
           {
             id: '', answer1: '', answer2: '', answer3: ''
@@ -24,7 +22,8 @@ class Body extends Component {
         },
 
         {
-          id: 3, question: 'What languages are spoken in the Philippines?',
+          id: 3,
+          question: 'What languages are spoken in the Philippines?',
           answers:
           {
             id: '', answer1: '', answer2: '', answer3: ''
@@ -32,7 +31,8 @@ class Body extends Component {
         },
 
         {
-          id: 4, question: 'What currency is used in the Philippines?',
+          id: 4,
+          question: 'What currency is used in the Philippines?',
           answers:
           {
             id: '', answer1: '', answer2: '', answer3: ''
@@ -40,7 +40,8 @@ class Body extends Component {
         },
 
         {
-          id: 5, question: 'What is the latitude and longitude for the Philippines?',
+          id: 5,
+          question: 'What is the latitude and longitude for the Philippines?',
           answers:
           {
             id: '', answer1: '', answer2: '', answer3: ''
@@ -48,7 +49,8 @@ class Body extends Component {
         },
 
         {
-          id: 6, question: 'What are the people of the Philippines also referenced as?',
+          id: 6,
+          question: 'What are the people of the Philippines also referenced as?',
           answers:
           {
             id: '', answer1: '', answer2: '', answer3: ''
@@ -56,7 +58,8 @@ class Body extends Component {
         },
 
         {
-          id: 7, question: 'What is the sub-region in which the Philippines is located?',
+          id: 7,
+          question: 'What is the sub-region in which the Philippines is located?',
           answers:
           {
             id: '', answer1: '', answer2: '', answer3: ''
@@ -64,7 +67,8 @@ class Body extends Component {
         },
 
         {
-          id: 8, question: 'What is the calling code for the Philippines?',
+          id: 8,
+          question: 'What is the calling code for the Philippines?',
           answers:
           {
             id: '', answer1: '', answer2: '', answer3: ''
@@ -72,7 +76,8 @@ class Body extends Component {
         },
 
         {
-          id: 9, question: 'How does one say the Philippines in German?',
+          id: 9,
+          question: 'How does one say the Philippines in German?',
           answers:
           {
             id: '', answer1: '', answer2: '', answer3: ''
@@ -80,20 +85,28 @@ class Body extends Component {
         },
 
         {
-          id: 10, question: 'What do they put on the end of their URL?',
+          id: 10,
+          question: 'What do they put on the end of their URL?',
           answers:
           {
             id: '', answer1: '', answer2: '', answer3: ''
           }
         }]
     }
-
-    render() {
-      return (
-        <div>
-        </div>
-      )
-    }
   }
 
-  export default Body
+  render () {
+    return (
+      <div className='body'>
+        <div className='questions-body'>
+          <p>{this.props.questions.map((question) => {
+            return question
+          })}</p>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+export default Body
